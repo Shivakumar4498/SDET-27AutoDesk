34750d88b36dae6f4cceee869a904222f542eb70
HP <HP@Shivakumar-Lap>
2021-12-30 10:42:56 +0530

    Example for buy

---
+++ com.crm.sdet-27/src/test/java/practice/ExampleGit.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class ExampleGit {
+
+	public static void main(String[] args) {
+		
+		
+			int value = 1000;
+			System.out.println("Value is 1000Rs");
+			int model = 3301;
+			System.out.println("Model is 3301");
+
+		}
+}
+	
+
+
